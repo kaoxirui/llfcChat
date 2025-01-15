46679eac356d7535233cc5b7697d24364d5d4945
kaoxirui <1106231574@qq.com>
2025-01-15 17:14:18 +0800

    test:测试jsoncpp库

---
+++ test/build/CMakeFiles/testjson.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kao/cpp/llfcChat/test/src/testjson.cpp" "/home/kao/cpp/llfcChat/test/build/CMakeFiles/testjson.dir/src/testjson.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/jsoncpp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
